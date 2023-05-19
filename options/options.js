d79cb54960948002f8e587823d4d7d95c003e56a
Crowdbotics <team@crowdbotics.com>
2023-05-19 12:32:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-mud-40841",
+  url: "https://dry-mud-40841.botics.co",
+  api: "https://dry-mud-40841.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
